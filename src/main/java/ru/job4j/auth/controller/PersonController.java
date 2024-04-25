3b16b1920b6cd7ad7f14eb27a56bb8e598e8bdbc
Rubezzz <inigor1@mail.ru>
2024-04-25 22:31:23 +0700

    8. Валидация моделей в Spring REST [#504801]

--- src/main/java/ru/job4j/auth/controller/PersonController.java
+++ src/main/java/ru/job4j/auth/controller/PersonController.java
@@ -7,16 +7,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.auth.domain.Person;
 import ru.job4j.auth.dto.PersonPasswordDto;
+import ru.job4j.auth.marker.Operation;
 import ru.job4j.auth.service.PersonService;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -44,20 +43,15 @@ public class PersonController {
     }
 
     @PostMapping("/sign-up")
-    public ResponseEntity<Person> create(@RequestBody Person person) {
-        if (person.getLogin().length() < 3) {
-            throw new IllegalArgumentException("Invalid login. Login length must be more than 3 characters.");
-        }
-        if (person.getPassword().length() < 6) {
-            throw new IllegalArgumentException("Invalid password. Password length must be more than 6 characters.");
-        }
+    @Validated(Operation.OnCreate.class)
+    public ResponseEntity<Person> create(@Valid @RequestBody Person person) {
         return persons.save(person)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.CONFLICT).build());
     }
 
     @PutMapping("/")
-    public ResponseEntity<Void> update(@RequestBody Person person) {
+    public ResponseEntity<Void> update(@Valid @RequestBody Person person) {
         return persons.update(person) ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
 
@@ -72,16 +66,5 @@ public class PersonController {
     @PatchMapping("/updatePass")
     public ResponseEntity<Person> updatePassword(@RequestBody PersonPasswordDto passwordDto) {
         return persons.updatePassword(passwordDto) ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public void exceptionHandler(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setStatus(HttpStatus.BAD_REQUEST.value());
-        response.setContentType("application/json");
-        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() { {
-            put("message", e.getMessage());
-            put("type", e.getClass());
-        }}));
-        LOGGER.error(e.getLocalizedMessage());
     }
 }
--- src/main/java/ru/job4j/auth/domain/Person.java
+++ src/main/java/ru/job4j/auth/domain/Person.java
@@ -2,11 +2,15 @@ package ru.job4j.auth.domain;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import ru.job4j.auth.marker.Operation;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -16,7 +20,11 @@ public class Person {
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @NotNull(message = "Id must be non null",
+            groups = { Operation.OnUpdate.class, Operation.OnDelete.class })
+    private Integer id;
+    @NotBlank(message = "Login must be not empty")
     private String login;
+    @Size(min = 6, message = "Password length must be more than 6 characters.")
     private String password;
 }
